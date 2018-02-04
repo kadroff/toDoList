9a091f52e8cc0557b83935ff38cce45c49d89363
kadroff <marsel.kadroff@gmail.com>
2018-02-05 01:07:11 +0500

    ToDoList 1.0

---
+++ app.js
@@ -0,0 +1,24 @@
+var toDoList = [];
+
+document.getElementById("add").onclick = function() {
+  var val = document.getElementById("in").value;
+  var list = {};
+  list.todo = val;
+  list.check = false;
+  var toDoList_len = toDoList.length;
+  toDoList[toDoList_len] = list;
+  out();
+}
+
+function out() {
+ var out = "";
+  for (key in toDoList) {
+    if (toDoList.check == true) {
+      out += "<input type='checkbox'> checked";
+    }else {
+      out += "<input type='checkbox'>";
+    }
+    out += toDoList[key].todo + "<br>";
+  }
+  document.getElementById("out").innerHTML = out;
+}
